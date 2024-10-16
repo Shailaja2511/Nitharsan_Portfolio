98b88ee275679a51621225466b84ae3b74dd7ffc
Shailaja2511 <shailajamaheswaran@gmail.com>
2024-10-16 22:20:34 +0530

    mobile view updated

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -112,19 +112,19 @@ if (bgImages) {
   6. Sliders
 ===============================================*/
 var swiper = new Swiper(".clients-swiper", {
-  slidesPerView: 2,
-  spaceBetween: 30,
+  slidesPerView: 5,
+  spaceBetween: 20,
   grabCursor: true,
   breakpoints: {
     // when window width is >= 768px
     768: {
-      slidesPerView: 3,
-      spaceBetween: 30
+      slidesPerView: 5,
+      spaceBetween: 20
     },
     // when window width is >= 992px
     992: {
-      slidesPerView: 3,
-      spaceBetween: 40
+      slidesPerView: 5,
+      spaceBetween: 20
     },
     // when window width is >= 1200px
     1200: {
